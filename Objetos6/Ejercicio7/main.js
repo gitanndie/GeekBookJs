afff6006f87ec14840b87b22dd9c2825298e70e0
Anndie Gonzalez <anndiegonzalez@gmail.com>
2020-08-24 08:24:32 -0500

    subiendo carpetas

---
+++ Objetos6/Ejercicio7/main.js
@@ -0,0 +1,12 @@
+const job = "Developer";
+let user = {
+  firstName: "Andrea",
+  lastName: "Gonzalez",
+  age: 30,
+  myjob: job,
+};
+
+console.log(user);
+user.firstName = "Quirama";
+user.age++;
+console.log(user);
